2768ba93051be5bc8ccfbc630279adce8e967bc2
yujiloh0816 <imp.yfujiwara@gmail.com>
2019-04-10 08:47:31 +0000

    [add]Viewsfiles

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/about'
   resources :books
-  resources :users
   devise_for :users
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
